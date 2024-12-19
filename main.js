567f65ea808be985eea3cab4a555ad423417b57d
islomjon <islomanvarov05@gmail.com>
2024-12-19 23:23:18 +0500

    vazifa bajarildi

---
+++ main.js
@@ -0,0 +1,167 @@
+// 1-masala
+
+
+// let A = +prompt("A kesmani kiriting!")
+// let B = +prompt("B kesmani kiriting")
+// let C = 0 //bu bitta bitta oshirib borish uchun yozildi!
+
+
+// while (A >= B) {
+//     A = A - B
+//     C++ 
+// }
+
+// console.log(`Bosh qolgan qismi:${A}`);
+
+
+// 2 - masala
+
+
+// let A = +prompt("A kesmani kiriting!")
+// let B = +prompt("B kesmani kiriting")
+// let C = 0 //bu bitta bitta oshirib borish uchun yozildi!
+
+
+// while (A >= B) {
+//     A = A - B
+//     C++ 
+// }
+
+// console.log(`A kemada B kesmani ${C} marta joylashtirish mumkin!`);
+
+
+// 3 - masala
+
+
+// let N = +prompt("N sonni kiriting!")
+// let K = +prompt("K sonni kiriting")
+// let C = 0 //bu bitta bitta oshirib borish uchun yozildi!
+
+
+// while (N >= K) {
+//     N = N - K
+//     C++ 
+// }
+
+// console.log(`Qoldiq:${N} butun qismi: ${C}`);
+
+
+// 4- masala
+
+// let n = +prompt("n sonini kiriting")
+// let c = 1
+
+
+// if (n > 0) {
+//     while (c < n) { 
+//         c *= 3;
+//     }
+//     console.log(c);
+
+//     if (c === n) {
+//         console.log("3 – ning darajasi");
+//     } else {
+//         console.log("3 – ning darajasi emas");
+//     }
+// } else {
+//     console.log("n musbat butun son bo'lishi kerak!");
+// }
+
+
+// 5 - masala
+// let n = +prompt("n sonini kiriting")
+// let k = 0
+// let m = 1
+
+
+// if (n > 0) {
+//     while (m < n) {
+//         m *= 2
+//         k++
+//     }
+// } else {
+//     console.log("xato kiritildi");
+// }
+
+// if (m === n) {
+//     m = "Bu ikki sonining darajasi"
+
+// } else {
+//     m = "bu ikki sonining darajasi emas"
+
+// }
+
+// console.log(m);
+
+
+// 6-masala
+//  let n = +prompt("n sonini kiriting")
+//  let k = 1
+
+
+//  if(n % 2 === 0) {
+//     while (n > 0) {
+//         k *= n
+//         n -= 2
+//     }
+//  } else {
+//     while (n > 0) {
+//         k *= n
+//         n -= 2
+//     }
+//  }
+
+
+//  console.log(k);
+
+
+// 7-masala
+
+// let n = +prompt("n ni kiriting (n > 0): ");
+
+// let k = 1;
+// while (k * k <= n) {
+//     k++;
+// }
+
+// console.log(`Kvadrati ${n} dan katta bo'lgan eng kichik butun k soni: ${k}`);
+
+
+// 8-masala
+
+// let n = +prompt("n ni kiriting (n > 0): ");
+
+// let k = 1;
+// while (k * k <= n) {
+//     k++;
+// }
+
+// k = k - 1
+
+// console.log(`Kvadrati ${n} dan kichik bo'lgan eng katta butun k soni: ${k}`);
+
+
+// 9 - masala
+
+// let n = +prompt("n ni kiriting (n > 0): ");
+
+// let k = 1;
+// while (Math.pow(k, 3) <= n) {
+//     k++;
+// }
+
+// k = k - 1
+
+// console.log(`kubi ${n} dan kichik bo'lgan eng katta butun k soni: ${k}`);
+
+
+// 10-masala
+
+// let n = +prompt("n ni kiriting (n > 0): ");
+
+// let k = 1;
+// while (Math.pow(k, 3) <= n) {
+//     k++;
+// }
+
+// console.log(`kubi ${n} dan kichik bo'lgan eng katta butun k soni: ${k}`);
